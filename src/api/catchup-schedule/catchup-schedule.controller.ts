13ef8db1c47ce117eae7110e2df6e51853bb4a2a
omadbeknasirullayev <omadbeknasirullayev4@gmail.com>
2025-12-12 15:15:18 +0500

    fix: catchup module errors fixed

--- src/api/catchup-schedule/catchup-schedule.controller.ts
+++ src/api/catchup-schedule/catchup-schedule.controller.ts
@@ -1,34 +1,39 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CatchupScheduleService } from './catchup-schedule.service';
-import { CreateCatchupScheduleDto } from './dto/create-catchup-schedule.dto';
-import { UpdateCatchupScheduleDto } from './dto/update-catchup-schedule.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from "@nestjs/common";
+import { CatchupScheduleService } from "./catchup-schedule.service";
+import { CreateCatchupScheduleDto } from "./dto/create-catchup-schedule.dto";
+import { UpdateCatchupScheduleDto } from "./dto/update-catchup-schedule.dto";
 
-@Controller('catchup-schedule')
+@Controller("catchup-schedule")
 export class CatchupScheduleController {
-  constructor(private readonly catchupScheduleService: CatchupScheduleService) {}
+	constructor(private readonly catchupScheduleService: CatchupScheduleService) {}
 
-  @Post()
-  create(@Body() createCatchupScheduleDto: CreateCatchupScheduleDto) {
-    return this.catchupScheduleService.create(createCatchupScheduleDto);
-  }
+	@Post()
+	create(@Body() createCatchupScheduleDto: CreateCatchupScheduleDto) {
+		return this.catchupScheduleService.create(createCatchupScheduleDto);
+	}
 
-  @Get()
-  findAll() {
-    return this.catchupScheduleService.findAll();
-  }
+	@Get()
+	findAll() {
+		return this.catchupScheduleService.findAll();
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.catchupScheduleService.findOne(+id);
-  }
+	@Get(":id")
+	findOne(@Param("id", ParseIntPipe) id: number) {
+		return this.catchupScheduleService.findOneBy({
+			where: { id, isDeleted: false, isActive: true },
+		});
+	}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCatchupScheduleDto: UpdateCatchupScheduleDto) {
-    return this.catchupScheduleService.update(+id, updateCatchupScheduleDto);
-  }
+	@Patch(":id")
+	update(
+		@Param("id", ParseIntPipe) id: number,
+		@Body() updateCatchupScheduleDto: UpdateCatchupScheduleDto,
+	) {
+		return this.catchupScheduleService.update(id, updateCatchupScheduleDto);
+	}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.catchupScheduleService.remove(+id);
-  }
+	@Delete(":id")
+	remove(@Param("id", ParseIntPipe) id: number) {
+		return this.catchupScheduleService.remove(+id);
+	}
 }
--- src/api/catchup-schedule/catchup-schedule.module.ts
+++ src/api/catchup-schedule/catchup-schedule.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { CatchupScheduleService } from './catchup-schedule.service';
-import { CatchupScheduleController } from './catchup-schedule.controller';
+import { Module } from "@nestjs/common";
+import { CatchupScheduleService } from "./catchup-schedule.service";
+import { CatchupScheduleController } from "./catchup-schedule.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CatchupSchedule } from "src/common/database/enity/catchup-schedule.entity";
 
 @Module({
-  controllers: [CatchupScheduleController],
-  providers: [CatchupScheduleService],
+	imports: [TypeOrmModule.forFeature([CatchupSchedule])],
+	controllers: [CatchupScheduleController],
+	providers: [CatchupScheduleService],
+	exports: [CatchupScheduleService],
 })
 export class CatchupScheduleModule {}
